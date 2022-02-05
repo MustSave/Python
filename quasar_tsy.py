654a368b506ce3b038a3b7c7b5dfefe175285c54
MustSave <58774251+MustSave@users.noreply.github.com>
2022-02-05 13:00:05 +0900

    퀘이사존 타세요 크롤링

---
+++ quasar_tsy.py
@@ -0,0 +1,60 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+import telegram as telepot
+
+def isnew(vic):
+    DIR = os.getcwd()+'/'
+    with open(DIR+"list.txt", 'r', -1, 'utf-8') as f:
+        prod = f.readline().strip()
+    
+    if prod == vic.strip():
+        return False, prod
+    else:
+        f = open(DIR+"list.txt", 'w', -1, 'utf-8')
+        f.write(vic)
+        f.close()
+        return True, prod
+
+def telegram(msg, token, id):
+    try:
+        bot = telepot.Bot(token)
+        bot.sendMessage(id, msg)
+    except:
+        pass
+            
+
+URL = "https://quasarzone.com/bbs/qb_tsy"
+N = ""
+
+
+req = requests.get(URL)
+html = req.text
+bs = BeautifulSoup(html, 'html.parser')
+
+
+
+table = bs.find("div", "market-type-list market-info-type-list relative")
+table = table.find("tbody").find_all("tr")
+
+for idx, tr in enumerate(table):
+    tr = tr.find('div', 'market-info-list-cont')
+
+    link = "https://quasarzone.com"+tr.find('a', 'subject-link')['href']
+    label = tr.find('span', 'label').text
+    name = tr.find('span', 'ellipsis-with-reply-cnt').text
+    price = tr.find('span', 'text-orange').text
+    
+    
+    
+    if idx == 0:
+        b, N = isnew(link)
+        if b == False:
+            break
+
+    if N.strip() == list.strip():
+        break
+    
+    res = label + "\n\n" + name + "\n" + price + "\n\n" + link
+    #print(res)
+    #telegram(res, "", '')
